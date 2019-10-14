0929bc0fb3e0eb38d0ed0968a3ba5a2df8712e6d
zhongyuhan <715181149@qq.com>
2019-10-14 13:38:02 +0800

    feat: 1

--- springboot-study/src/main/java/com/itmayiedu/member/controller/App.java
+++ springboot-study/src/main/java/com/itmayiedu/member/controller/App.java
@@ -21,6 +21,8 @@ public class App {
         SpringApplication.run(App.class, args);
     }
 
+
+
 }
 
 /**
--- springboot-study/src/main/java/com/itmayiedu/member/controller/controller/ErrorController.java
+++ springboot-study/src/main/java/com/itmayiedu/member/controller/controller/ErrorController.java
@@ -1,14 +1,19 @@
 package com.itmayiedu.member.controller.controller;
 
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ErrorController {
-
+    @ApiOperation(value = "资产状态跃迁", notes = "传入实体对象信息")
+    @ApiResponses(value = { @ApiResponse(code = 200, message = "OK", response = Integer.class, responseContainer = "actionResponse"), })
     @RequestMapping("/getUser")
-    String getUser(int i){
+    String getUser(@ApiParam(value = "statusJumpRequest")int i){
         int j=1/i;
         return "success"+j;
     }
